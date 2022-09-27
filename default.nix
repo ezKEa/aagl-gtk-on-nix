d9f827028e68a60d0611f6c5bc34f4b71c4aa3a2
ezKEa <wrp7bapuk@mozmail.com>
2022-09-27 19:02:50 -0400

    module: add module! :)

--- default.nix
+++ default.nix
@@ -3,6 +3,7 @@ let
   sources = import ./nix/sources.nix { };
 in
 rec {
+  module = import ./module;
   an-anime-game-launcher-gtk = pkgs.callPackage ./pkgs/an-anime-game-launcher-gtk { inherit an-anime-game-launcher-gtk-unwrapped; };
   an-anime-game-launcher-gtk-unwrapped = pkgs.callPackage ./pkgs/an-anime-game-launcher-gtk/unwrapped.nix { libadwaita = libadwaita12; };
   libadwaita12 = (import sources.nixpkgs { }).libadwaita;
---
+++ module/default.nix
@@ -0,0 +1,46 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  an-anime-game-launcher-gtk = (import ../default.nix { inherit pkgs; }).an-anime-game-launcher-gtk;
+  cfg = config.programs.an-anime-game-launcher;
+in
+{
+  options.programs.an-anime-game-launcher = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = lib.mdDoc ''
+        Whether to enable an-anime-game-launcher.
+      '';
+    };
+    package = mkOption {
+      type = types.package;
+      default = an-anime-game-launcher-gtk;
+      description = lib.mdDoc ''
+        an-anime-game-launcher package to use.
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [ cfg.package ];
+    networking.extraHosts = ''
+      # Logging servers (do not remove!)
+      0.0.0.0 overseauspider.yuanshen.com
+      0.0.0.0 log-upload-os.hoyoverse.com
+
+      # Logging servers (do not remove!)
+      0.0.0.0 log-upload.mihoyo.com
+      0.0.0.0 uspider.yuanshen.com
+
+      # Optional Unity proxy/cdn servers
+      0.0.0.0 prd-lender.cdp.internal.unity3d.com
+      0.0.0.0 thind-prd-knob.data.ie.unity3d.com
+      0.0.0.0 thind-gke-usc.prd.data.corp.unity3d.com
+      0.0.0.0 cdp.cloud.unity3d.com
+      0.0.0.0 remote-config-proxy-prd.uca.cloud.unity3d.com
+    '';
+  };
+}

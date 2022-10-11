093da98c0fdb511d23ea306d5dcbc4e1f71afd86
ezKEa <wrp7bapuk@mozmail.com>
2022-10-11 13:34:20 -0400

    an-anime-game-launcher-gtk: add .desktop entry

--- pkgs/an-anime-game-launcher-gtk/default.nix
+++ pkgs/an-anime-game-launcher-gtk/default.nix
@@ -7,6 +7,8 @@
 , writeShellScriptBin
 , xdelta
 , runtimeShell
+, stdenv
+, makeDesktopItem
 , an-anime-game-launcher-gtk-unwrapped
 }:
 let
@@ -42,10 +44,27 @@ let
   wrapper = writeShellScriptBin "anime-game-launcher" ''
     ${steam-run-custom}/bin/steam-run ${an-anime-game-launcher-gtk-unwrapped}/bin/anime-game-launcher "$@"
   '';
+
+  icon = stdenv.mkDerivation {
+    name = "An Anime Game Launcher icon";
+    buildCommand = ''
+      mkdir -p $out/share/pixmaps
+      cp ${an-anime-game-launcher-gtk-unwrapped.src}/assets/images/icon.png $out/share/pixmaps/aagl-gtk.png
+    '';
+  };
+
+  desktopEntry = makeDesktopItem {
+    name = "anime-game-launcher";
+    desktopName = "An Anime Game Launcher GTK";
+    genericName = "Anime Game Launcher";
+    exec = "${wrapper}/bin/anime-game-launcher";
+    icon = "aagl-gtk";
+    startupNotify = true;
+  };
 in
 symlinkJoin {
   inherit (an-anime-game-launcher-gtk-unwrapped) pname version name;
-  paths = [ wrapper ];
+  paths = [ icon desktopEntry wrapper ];
 
   meta = with lib; {
     description = "An Anime Game Launcher variant written on Rust, GTK4 and libadwaita, using Anime Game Core library";

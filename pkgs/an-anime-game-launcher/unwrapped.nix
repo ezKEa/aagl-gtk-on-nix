0464d63e2f3dd011f399882440b76b59be989072
ezKEa <wrp7bapuk@mozmail.com>
2023-03-07 02:56:51 -0500

    Revert "an-anime-game-launcher: fix indeterminism"
    This reverts commit 5f36f2d2a3e9c57f15ab5b3fe1ec0251ca4cf090.

--- pkgs/an-anime-game-launcher/unwrapped.nix
+++ pkgs/an-anime-game-launcher/unwrapped.nix
@@ -6,11 +6,11 @@ lib, rustPlatform, fetchFromGitHub
 , pango
 , gdk-pixbuf
 , gtk4
+, git
 , libadwaita
 , gobject-introspection
 , gsettings-desktop-schemas
 , wrapGAppsHook4
-, writeShellScriptBin
 , librsvg
 
 , customIcon ? null
@@ -25,21 +25,9 @@ rustPlatform.buildRustPackage rec {
     owner = "an-anime-team";
     repo = "an-anime-game-launcher";
     rev = version;
-    sha256 = "sha256-MjXOx+lsNBOJAo1FcoPPPpS7XKwDT2Yw+9hdOm+Opwk=";
+    sha256 = "sha256-p78gvK7FPuslSHGDgYZoWGOxr/1tzIhfEpr6s4LH0t0=";
     fetchSubmodules = true;
     leaveDotGit = true;
-    # leaveDotGit causes indeterminism
-    # https://github.com/NixOS/nixpkgs/issues/8567
-    # Need git commits hashes for about menu, so we'll get those and save them
-    postFetch = ''
-      buildDir="$PWD"
-      find "$out" -type d -name .git | while read -r dir; do
-        cd "$dir/.."
-        echo "$(git rev-parse HEAD)" "$(basename $PWD)" >> $buildDir/COMMITS
-      done
-      find "$out" -name .git -print0 | xargs -0 rm -rf
-      cat $buildDir/COMMITS | tail -n +2 > $out/COMMITS
-    '';
   };
 
   prePatch = optionalString (customIcon != null) ''
@@ -49,14 +37,8 @@ rustPlatform.buildRustPackage rec {
 
   cargoSha256 = "sha256-d1vdjvHLFz0DRepECtuo064lIKdnYPV0sPjyXKH3fYU=";
 
-  nativeBuildInputs = let
-    fakeGit = writeShellScriptBin "git" ''
-      set -e
-      cat $src/COMMITS | grep "$(basename $PWD)" | awk '{ print $1 }'
-    '';
-  in
-  [
-    fakeGit
+  nativeBuildInputs = [
+    git
     glib
     gobject-introspection
     gtk4

32865b18d0263ae5498b57ab674b3dff0bf300fd
ezKEa <wrp7bapuk@mozmail.com>
2023-03-07 09:35:23 -0500

    an-anime-game-launcher: 3.1.1 -> 3.1.2

--- pkgs/an-anime-game-launcher/unwrapped.nix
+++ pkgs/an-anime-game-launcher/unwrapped.nix
@@ -19,13 +19,13 @@ lib, rustPlatform, fetchFromGitHub
 with lib;
 rustPlatform.buildRustPackage rec {
   pname = "an-anime-game-launcher";
-  version = "3.1.1";
+  version = "3.1.2";
 
   src = fetchFromGitHub {
     owner = "an-anime-team";
     repo = "an-anime-game-launcher";
     rev = version;
-    sha256 = "sha256-6OCUB6KuV0EV172o526UibHvOUDsU1FXx0Vvz0KIYgM=";
+    sha256 = "sha256-+docyWhrmxbKD0fnbCQc1ilo/vvRY9qaP8sjgK73e7I=";
     fetchSubmodules = true;
   };
 
@@ -34,17 +34,17 @@ rustPlatform.buildRustPackage rec {
     cp ${customIcon} assets/images/icon.png
   '';
 
-  cargoSha256 = "sha256-d1vdjvHLFz0DRepECtuo064lIKdnYPV0sPjyXKH3fYU=";
+  cargoSha256 = "sha256-loKrExbu0LnW57gkTTCJIOrrISIpmW0wSKBjXmlx6Jw=";
 
   nativeBuildInputs = let
     # Later do this the right way
     fakeGit = writeShellScriptBin "git" ''
       dir="$(basename $PWD)"
       if [[ "$dir" == "anime-launcher-sdk" ]]; then
-        echo "219d0f7704e18d74a9205163ffbb7d1718291ea4";
+        echo "7bcfdbee8583f046c9df045577bd679ee4eb45c2";
       fi
       if [[ "$dir" == "anime-game-core" ]]; then
-        echo "45894e77c9ec1fcfe264c80f6a39774876413386";
+        echo "7593e6ba4bdffe1e5de27e8010abfa309633207a";
       fi
     '';
   in

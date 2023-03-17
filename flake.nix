e82ff160c885c4bb2b529661a6980dbe821c307a
yavko <yavornkolev@gmail.com>
2023-03-17 00:03:15 -0700

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs = {nixpkgs, ...}: let
+    inherit (nixpkgs) lib;
+    genSystems = lib.genAttrs [
+      # Supported OSes
+      "x86_64-linux"
+    ];
+  in {
+    nixosModules.default = import ./module/default.nix; #(genSystems (system: nixpkgs.legacyPackages.${system}));
+    nixConfig = {
+      extra-substituters = ["https://ezkea.cachix.org"];
+      extra-trusted-public-keys = ["ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="];
+    };
+  };
+}

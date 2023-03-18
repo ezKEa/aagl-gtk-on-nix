61ef47c0e6b370a54a2fea9756b5952b9406b551
yavko <yavornkolev@gmail.com>
2023-03-18 14:04:42 -0700

    Add overlay and packages

--- flake.nix
+++ flake.nix
@@ -8,11 +8,21 @@
       # Supported OSes
       "x86_64-linux"
     ];
+    unwrapped = pkgs: pkgs.callPackage ./pkgs/an-anime-game-launcher/unwrapped.nix {};
+    regular = pkgs: pkgs.callPackage ./pkgs/an-anime-game-launcher/default.nix {an-anime-game-launcher-unwrapped = unwrapped pkgs;};
   in {
     nixosModules.default = import ./module/default.nix;
     nixConfig = {
       extra-substituters = ["https://ezkea.cachix.org"];
       extra-trusted-public-keys = ["ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="];
+    };
+    packages = genSystems (system: {
+      unwrapped = unwrapped nixpkgs.legacyPackages.${system};
+      default = regular nixpkgs.legacyPackages.${system};
+    });
+    overlays.default = _: prev: {
+      an-anime-game-launcher-unwrapped = unwrapped prev;
+      an-anime-game-launcher = regular prev;
     };
   };
 }

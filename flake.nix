8a95f6585736defb765247050aac47d9c2ea79ff
ezKEa <wrp7bapuk@mozmail.com>
2023-12-22 16:05:34 -0500

    flake.lock: use nixos-23.11 instead of unstable

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@ rec {
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;

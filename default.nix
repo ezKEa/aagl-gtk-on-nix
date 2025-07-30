e276a9e6312cdaf58a979f9cad8709c83bd7e67c
ezKEa <wrp7bapuk@mozmail.com>
2025-07-29 21:40:30 -0400

    cleanup compat stuff

--- default.nix
+++ default.nix
@@ -1,5 +1,17 @@
 let
-  flake = import ./compat.nix;
+
+  flake = (import (
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    in
+      fetchTarball {
+        url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+        sha256 = lock.nodes.flake-compat.locked.narHash;
+      }
+    ) {
+      src = ./.;
+    }
+  ).defaultNix;
 
   overlay = flake.outputs.overlays.default;
 
@@ -16,6 +28,6 @@ let
   in packages // {
     inherit overlay;
     inherit (flake.outputs) nixConfig;
-    module = import ./module flake;
+    module = flake.outputs.nixosModules.default;
   };
 in outputs

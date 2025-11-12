888b761c7ac5554f0a34ec5b34a95151507e6534
ezKEa <wrp7bapuk@mozmail.com>
2025-11-12 09:42:09 -0500

    honkers-launcher: 1.12.0 -> 1.13.0

--- pkgs/honkers-launcher/unwrapped.nix
+++ pkgs/honkers-launcher/unwrapped.nix
@@ -18,13 +18,13 @@
 }:
 rustPlatform.buildRustPackage (self: {
   pname = "honkers-launcher";
-  version = "1.12.0";
+  version = "1.13.0";
 
   src = fetchFromGitHub {
     owner = "an-anime-team";
     repo = self.pname;
     rev = self.version;
-    sha256 = "sha256-bOvGaBWi5EHJ4oRFurzcY1Y8ZOU5KaLVOSn1/+WQw0c=";
+    sha256 = "sha256-df32blPQjXNbQ9CqVfuxDf7c1UB4fiub+KY9a22CvrY=";
     fetchSubmodules = true;
   };
 
@@ -33,7 +33,7 @@ rustPlatform.buildRustPackage (self: {
     cp ${customIcon} assets/images/icon.png
   '';
 
-  cargoHash = "sha256-ac8cGqMVXtiyn745/tZ/Q3y4S6qXwskLPaTDvfi26BE=";
+  cargoHash = "sha256-9B/CvpMrKKguhZ93dPkGjTeTSjVsNwUw+mZX/bjE+ng=";
 
   nativeBuildInputs = [
     cmake

e272196d003064dfb177ed9cae6a12021b806048
ezKEa <wrp7bapuk@mozmail.com>
2025-11-26 09:53:18 -0500

    25.11: branch off from main

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@ rec {
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-compat = {
       url = "github:edolstra/flake-compat";

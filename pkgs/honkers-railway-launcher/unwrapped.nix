42d2d8a6b1b805b815a3ce916a65d1b748667c39
ezKEa <wrp7bapuk@mozmail.com>
2025-12-17 10:20:06 -0500

    the-honkers-railway-launcher: 1.13.0 -> 1.14.0

--- pkgs/honkers-railway-launcher/unwrapped.nix
+++ pkgs/honkers-railway-launcher/unwrapped.nix
@@ -5,6 +5,7 @@
   pkg-config,
   cmake,
   openssl,
+  protobuf,
   glib,
   pango,
   gdk-pixbuf,
@@ -18,13 +19,13 @@
 }:
 rustPlatform.buildRustPackage (self: {
   pname = "honkers-railway-launcher";
-  version = "1.13.0";
+  version = "1.14.0";
 
   src = fetchFromGitHub {
     owner = "an-anime-team";
     repo = "the-honkers-railway-launcher";
     rev = self.version;
-    sha256 = "sha256-DyOAXSkfOm3di9yujZVhu3+r3F9W8eKNi/A7yUGdKiU=";
+    sha256 = "sha256-6PJ663156B897jVb0NP7at3bM0+75trx0g4+bW8uleA=";
     fetchSubmodules = true;
   };
 
@@ -33,10 +34,11 @@ rustPlatform.buildRustPackage (self: {
     cp ${customIcon} assets/images/icon.png
   '';
 
-  cargoHash = "sha256-uSQ3kjT/UcAJ4/xzfe+sTiwm7zRnuRmbBSAhrGQGbBo=";
+  cargoHash = "sha256-Isf4W75ELZnPVFW1aXb34Z5+27WGEskoGI081Ltx9+k=";
 
   nativeBuildInputs = [
     cmake
+    protobuf
     glib
     gobject-introspection
     gtk4
